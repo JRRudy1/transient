6692f7df177049e921c9a4756bb0306eba779779
Joshua Rudolph <jrrudolph93@gmail.com>
2024-01-25 15:37:01 -0600

    Expanded/updated documentation. Added safe methods to the wrapper structs that expose the inner value if the true lifetime is 'static.

--- src/erased.rs
+++ src/erased.rs
@@ -1,4 +1,17 @@
-//! Defines wrapper structs providing a safe interface for erased objects.
+//! Defines safe wrappers for erasing and restoring transient types.
+//!
+//! This module defines the `Erased`, `ErasedRef`, and `ErasedMut` structs for
+//! wrapping owned values, shared references, and mutable references, respectively,
+//! that have been transmuted to `'static` and cast to `dyn Any`. While artificially
+//! extending lifetimes is typically very unsafe, each wrapper struct provides a
+//! safe interface to the falsely-`'static` value it wraps by restricting safe
+//! access to it until the *true* lifetime has been restored.
+//!
+//! In order to enforce this restriction, the safe public API does not expose the
+//! wrapped value directly, which in principle could be downcast and cloned to
+//! obtain a transient value with an unbounded lifetime. However, this restriction
+//! is lifted when the *true* lifetime is static, since use-after-free is no longer
+//! a concern.
 use std::{
     any::{Any, TypeId}, 
     marker::PhantomData
@@ -6,17 +19,19 @@ use std::{
 use super::MakeStatic;
 
 
-/// Wraps a (potentially) non-static object that has been
-/// transmuted to 'static and cast to `dyn Any`. This struct provides a
-/// safe interface for erasing/restoring such a type by restricting
-/// access to the falsely-static object and ensuring that it cannot be
-/// used after its true lifetime. To enforce this condition, the safe
-/// public API does not expose the wrapped reference directly, which
-/// in principle could be downcast and then cloned to produce an owned
-/// value capable of invoking UB from safe code (by using it after the
-/// true lifetime).
+/// Safely wraps an owned potentially non-static value that has been transmuted
+/// to `'static` and cast to `dyn Any`.
+///
+/// This struct provides a safe interface for erasing/restoring such a type by
+/// restricting access to the falsely-static object and ensuring that it cannot
+/// be used after its true lifetime. To enforce this condition, the safe public
+/// API does not expose the wrapped value directly, which in principle could be
+/// downcast and cloned to obtain a transient value with an unbounded lifetime.
 #[derive(Debug)]
-pub struct Erased<'src>(Box<dyn Any>, PhantomData<&'src ()>);
+pub struct Erased<'src>(
+    Box<dyn Any>,  // DO NOT EXPOSE!
+    PhantomData<&'src ()>
+);
 
 impl<'src> Erased<'src> {
 
@@ -51,20 +66,34 @@ impl<'src> Erased<'src> {
     }
 }
 
-/// Wraps a reference to a (potentially) non-static object that has been
-/// transmuted to 'static and cast to `dyn Any`. This struct provides a
-/// safe interface for erasing/restoring such a type by restricting
-/// access to the falsely-static object and ensuring that it cannot be
-/// used after its true lifetime. To enforce this condition, the safe
-/// public API does not expose the wrapped reference directly, which
-/// in principle could be downcast and then cloned to produce an owned
-/// value capable of invoking UB from safe code (by using it after the
-/// true lifetime).
+/// If the wrapped object is `'static` we can safely expose it.
+impl Erased<'static> {
+    pub fn inner(&self) -> &dyn Any {
+        &*self.0
+    }
+    pub fn inner_mut(&mut self) -> &mut dyn Any {
+        &mut *self.0
+    }
+    pub fn into_inner(self) -> Box<dyn Any> {
+        self.0
+    }
+}
+
+
+/// Safely wraps a shared reference to a potentially non-static value
+/// that has been transmuted to `'static` and cast to `dyn Any`.
+///
+/// This struct provides a safe interface for erasing/restoring such a type by
+/// restricting access to the falsely-static object and ensuring that it cannot
+/// be used after its true lifetime. To enforce this condition, the safe public
+/// API does not expose the wrapped value directly, which in principle could be
+/// downcast and cloned to obtain a transient value with an unbounded lifetime.
 #[derive(Debug, Clone, Copy)]
 pub struct ErasedRef<'borrow, 'src: 'borrow>(
-    &'borrow dyn Any,
+    &'borrow dyn Any,  // DO NOT EXPOSE!
     PhantomData<&'src ()>,
 );
+
 impl<'borrow, 'src: 'borrow> ErasedRef<'borrow, 'src> {
 
     pub fn new<T: MakeStatic<'src>>(value: &'borrow T) -> Self {
@@ -89,15 +118,22 @@ impl<'borrow, 'src: 'borrow> ErasedRef<'borrow, 'src> {
     }
 }
 
-/// Wraps a reference to a (potentially) non-static object that has been
-/// transmuted to 'static and cast to `dyn Any`. This struct provides a
-/// safe interface for erasing/restoring such a type by restricting
-/// access to the falsely-static object and ensuring that it cannot be
-/// used after its true lifetime. To enforce this condition, the safe
-/// public API does not expose the wrapped reference directly, which
-/// in principle could be downcast and then cloned to produce an owned
-/// value capable of invoking UB from safe code (by using it after the
-/// true lifetime).
+/// If the wrapped object is `'static` we can safely expose it.
+impl<'borrow> ErasedRef<'borrow, 'static> {
+    pub fn inner(&self) -> &dyn Any {
+        &*self.0
+    }
+}
+
+
+/// Safely wraps a mutable reference to a potentially non-static value
+/// that has been transmuted to `'static` and cast to `dyn Any`.
+///
+/// This struct provides a safe interface for erasing/restoring such a type by
+/// restricting access to the falsely-static object and ensuring that it cannot
+/// be used after its true lifetime. To enforce this condition, the safe public
+/// API does not expose the wrapped value directly, which in principle could be
+/// downcast and cloned to obtain a transient value with an unbounded lifetime.
 #[derive(Debug)]
 pub struct ErasedMut<'borrow, 'src: 'borrow>(
     &'borrow mut dyn Any,
@@ -125,5 +161,15 @@ impl<'borrow, 'src: 'borrow> ErasedMut<'borrow, 'src> {
 
     pub fn is<T: MakeStatic<'src>>(&self) -> bool {
         (&*self.0).is::<T::Static>()
+    }
+}
+
+/// If the wrapped object is `'static` we can safely expose it.
+impl<'borrow> ErasedMut<'borrow, 'static> {
+    pub fn inner(&self) -> &dyn Any {
+        &*self.0
+    }
+    pub fn inner_mut(&mut self) -> &mut dyn Any {
+        &mut *self.0
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
 //! This library provides a mechanism for safely casting non-static data to/from `dyn Any`.
 
-// TODO: reason about advanced cases (contravariance, multithreading, etc.)
-
-
 #[cfg(test)]
 pub mod tests;
 
 mod erased;
-
 pub use erased::{Erased, ErasedRef, ErasedMut};
 
 #[cfg(feature = "derive")]
@@ -16,34 +12,36 @@ pub use transient_any_derive::MakeStatic;
 
 /// Trait providing safe operations on `MakeStatic`  types.
 pub trait Erase<'src>: MakeStatic<'src> {
-    /// Erase the value's type and return an interface for safely restoring it.
+    /// Erase the value's type and return a wrapper for safely restoring it.
     fn into_erased(self) -> Erased<'src> {
         Erased::new(self)
     }
-    /// Erase the pointed-to value's type and return an interface for safely restoring it.
+    /// Erase the pointed-to value's type and return a wrapper for safely restoring it.
     fn as_erased<'borrow>(&'borrow self) -> ErasedRef<'borrow, 'src>
     where 'src: 'borrow {
         ErasedRef::new(self)
     }
-    /// Erase the pointed-to value's type and return an interface for safely restoring it.
+    /// Erase the pointed-to value's type and return a wrapper for safely restoring it.
     fn as_erased_mut<'borrow>(&'borrow mut self) -> ErasedMut<'borrow, 'src>
     where 'src: 'borrow {
         ErasedMut::new(self)
     }
     fn type_id() -> std::any::TypeId {
-        std::any::TypeId::of::<Self::Static>()
+        Self::static_type_id()
     }
 }
 impl<'src, T: MakeStatic<'src>> Erase<'src> for T {}
 
 
 /// An unsafe trait for converting the lifetime parameters of a type to
-/// (and from) 'static so that it can be cast to `dyn Any`.
+/// (and from) `'static` so that it can be cast to `dyn Any`.
 ///
 /// Only the associated type `Static` must be implemented, which must
 /// be the same as `Self` but with all lifetime parameters replaced by
-/// 'static. For example, a struct `S<'a, 'b, T>` should define `Static`
-/// as `S<'static, 'static, T>`.
+/// 'static. For example, a struct `S<'a, 'b, T>` should define the type
+/// as `S<'static, 'static, T>`. For structs with zero or one lifetime
+/// parameters, the included derive macro may be used to correctly
+/// implement this trait.
 ///
 /// SAFETY: `Self::Static` must be a static version of `Self` with the
 /// same layout, and `'src` must match the shortest lifetime parameter
@@ -55,39 +53,39 @@ pub unsafe trait MakeStatic<'src>: Sized + 'src {
         std::any::TypeId::of::<Self::Static>()
     }
 
-    /// SAFETY: the returned value must not be used after 'src
+    /// SAFETY: the returned value must not be used after `'src`; in
+    /// particular, it must not be cloned to produce an owned value
+    /// borrowing data with a falsely 'static lifetime.
     unsafe fn make_static_owned(self: Box<Self>) -> Box<Self::Static> {
         std::mem::transmute(self)
     }
 
-    /// SAFETY: the given value must be valid for 'src
+    /// SAFETY: the given value must be valid for `'src`
     unsafe fn from_static_owned(value: Box<Self::Static>) -> Box<Self> {
         std::mem::transmute(value)
     }
 
-    /// SAFETY: the returned value must not be used after 'src; in
+    /// SAFETY: the returned value must not be used after `'src`; in
     /// particular, it must not be cloned to produce an owned value
     /// borrowing data with a falsely 'static lifetime.
     unsafe fn make_static_ref(&self) -> &Self::Static {
         std::mem::transmute(self)
     }
 
-    /// SAFETY: the given value must be valid for 'src
+    /// SAFETY: the given value must be valid for `'src`
     unsafe fn from_static_ref<'b>(value: &'b Self::Static) -> &'b Self {
         std::mem::transmute(value)
     }
 
-    /// SAFETY: the returned value must not be used after 'src; in
+    /// SAFETY: the returned value must not be used after `'src`; in
     /// particular, it must not be cloned to produce an owned value
     /// borrowing data with a falsely 'static lifetime.
     unsafe fn make_static_mut(&mut self) -> &mut Self::Static {
         std::mem::transmute(self)
     }
 
-    /// SAFETY: the given value must be valid for 'src
-    // unsafe fn from_static_mut<'borrow>(value: &'borrow mut Self::Static) -> &'borrow mut Self
-    unsafe fn from_static_mut<'b>(value: &'b mut Self::Static) -> &'b mut Self
-    {
+    /// SAFETY: the given value must be valid for `'src`
+    unsafe fn from_static_mut<'b>(value: &'b mut Self::Static) -> &'b mut Self {
         std::mem::transmute(value)
     }
 }
