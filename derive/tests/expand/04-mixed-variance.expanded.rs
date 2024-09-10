4b9e2bced799642cc35b73ba49aca80eba11fd59
Matthew Pomes <matthew.pomes@pm.me>
2024-09-09 21:06:15 -0500

    Update macro expand with marginal change

--- derive/tests/expand/04-mixed-variance.expanded.rs
+++ derive/tests/expand/04-mixed-variance.expanded.rs
@@ -12,28 +12,30 @@ where
     type Static = ContraCo<'static, 'static, T>;
     type Transience = (::transient::Contra<'a>, ::transient::Co<'b>);
 }
-mod __validate_ContraCo {
-    #![allow(non_snake_case, dead_code)]
-    use super::*;
-    fn contravariant_wrt_a<'__short, 'a, 'b, T>(
-        v: ContraCo<'__short, 'b, T>,
-    ) -> ContraCo<'a, 'b, T>
-    where
-        T: 'static,
-        'a: '__short,
-    {
-        v
+const _: () = {
+    mod validate_ContraCo {
+        #![allow(non_snake_case, dead_code)]
+        use super::*;
+        fn contravariant_wrt_a<'__short, 'a, 'b, T>(
+            v: ContraCo<'__short, 'b, T>,
+        ) -> ContraCo<'a, 'b, T>
+        where
+            T: 'static,
+            'a: '__short,
+        {
+            v
+        }
+        fn covariant_wrt_b<'__long, 'a, 'b, T>(
+            v: ContraCo<'a, '__long, T>,
+        ) -> ContraCo<'a, 'b, T>
+        where
+            T: 'static,
+            '__long: 'b,
+        {
+            v
+        }
     }
-    fn covariant_wrt_b<'__long, 'a, 'b, T>(
-        v: ContraCo<'a, '__long, T>,
-    ) -> ContraCo<'a, 'b, T>
-    where
-        T: 'static,
-        '__long: 'b,
-    {
-        v
-    }
-}
+};
 #[covariant(a)]
 struct CoInv<'a, 'b, T1, T2> {
     value1: &'a T1,
@@ -47,20 +49,22 @@ where
     type Static = CoInv<'static, 'static, T1, T2>;
     type Transience = (::transient::Co<'a>, ::transient::Inv<'b>);
 }
-mod __validate_CoInv {
-    #![allow(non_snake_case, dead_code)]
-    use super::*;
-    fn covariant_wrt_a<'__long, 'a, 'b, T1, T2>(
-        v: CoInv<'__long, 'b, T1, T2>,
-    ) -> CoInv<'a, 'b, T1, T2>
-    where
-        T1: 'static,
-        T2: 'static,
-        '__long: 'a,
-    {
-        v
+const _: () = {
+    mod validate_CoInv {
+        #![allow(non_snake_case, dead_code)]
+        use super::*;
+        fn covariant_wrt_a<'__long, 'a, 'b, T1, T2>(
+            v: CoInv<'__long, 'b, T1, T2>,
+        ) -> CoInv<'a, 'b, T1, T2>
+        where
+            T1: 'static,
+            T2: 'static,
+            '__long: 'a,
+        {
+            v
+        }
     }
-}
+};
 #[covariant]
 struct GlobalCo<'a, 'b, T1, T2> {
     value1: &'a T1,
@@ -74,27 +78,29 @@ where
     type Static = GlobalCo<'static, 'static, T1, T2>;
     type Transience = (::transient::Co<'a>, ::transient::Co<'b>);
 }
-mod __validate_GlobalCo {
-    #![allow(non_snake_case, dead_code)]
-    use super::*;
-    fn covariant_wrt_a<'__long, 'a, 'b, T1, T2>(
-        v: GlobalCo<'__long, 'b, T1, T2>,
-    ) -> GlobalCo<'a, 'b, T1, T2>
-    where
-        T1: 'static,
-        T2: 'static,
-        '__long: 'a,
-    {
-        v
+const _: () = {
+    mod validate_GlobalCo {
+        #![allow(non_snake_case, dead_code)]
+        use super::*;
+        fn covariant_wrt_a<'__long, 'a, 'b, T1, T2>(
+            v: GlobalCo<'__long, 'b, T1, T2>,
+        ) -> GlobalCo<'a, 'b, T1, T2>
+        where
+            T1: 'static,
+            T2: 'static,
+            '__long: 'a,
+        {
+            v
+        }
+        fn covariant_wrt_b<'__long, 'a, 'b, T1, T2>(
+            v: GlobalCo<'a, '__long, T1, T2>,
+        ) -> GlobalCo<'a, 'b, T1, T2>
+        where
+            T1: 'static,
+            T2: 'static,
+            '__long: 'b,
+        {
+            v
+        }
     }
-    fn covariant_wrt_b<'__long, 'a, 'b, T1, T2>(
-        v: GlobalCo<'a, '__long, T1, T2>,
-    ) -> GlobalCo<'a, 'b, T1, T2>
-    where
-        T1: 'static,
-        T2: 'static,
-        '__long: 'b,
-    {
-        v
-    }
-}
+};
