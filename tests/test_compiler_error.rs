76e288cb039ea9f7fd87db2db02d750d185be651
Joshua Rudolph <JRRudolph93@gmail.com>
2024-12-09 12:04:12 -0600

    Updated `compile_fail` output to match that of the latest stable rust version, and gated the `compile_fail` tests to only run on recent versions.

--- tests/test_compiler_error.rs
+++ tests/test_compiler_error.rs
@@ -1,6 +1,7 @@
 //! Entry-point for tests that should fail to compile with an expected error message
 
-#[rustversion::stable]
+// #[rustversion::stable]
+#[rustversion::all(since(1.82), stable)]
 #[test]
 #[cfg_attr(miri, ignore)]
 fn variance_tests() {
